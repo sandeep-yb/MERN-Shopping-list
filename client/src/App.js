aa0dadf001a4e173af44d12c44732d3f9b2fde04
sandeep <sandeepkumaraadha@gmail.com>
2020-10-16 11:41:03 +0530

    Client Navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
-    
+      <NavBar/>
+
     </div>
   );
 }
